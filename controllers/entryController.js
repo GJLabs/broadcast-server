6b0f8d0a5af6de68bfb729b61074bc9db8fcbbd7
cassac <hicassac@gmail.com>
2016-09-16 22:22:12 -0700

    refactors to asynchronously store files

--- controllers/entryController.js
+++ controllers/entryController.js
@@ -12,27 +12,31 @@ module.exports = {
   createEntry: function(req, res, next){
 
     var form = new multiparty.Form();
-
-    new Promise((resolve, reject) => {
       
-      // initialize entry object with userId
-      var userId = req.user.id;
-      var entry = {userId: userId};
+    // initialize entry object with userId
+    var userId = req.user.id;
+    var entry = {userId: userId};
 
-      form.parse(req, (err, fields, files) => {
-        if (err) {
-          reject(err);
-        }
-        // Store entry text and location on entry object
-        entry.text = fields.text[0];
-        entry.location = fields.location[0];
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        reject(err);
+      }
 
-        for (var file in files) {
-          var filedata = files[file][0];
-          var temppath = filedata.path;
-          var filetype = filedata.headers['content-type'].split('/')
+      // Store entry text and location on entry object
+      entry.text = fields.text[0];
+      entry.location = fields.location[0];
+
+      var filesArray = files.file;
+      var fileIndex = filesArray.length - 1;
+
+      function parseFiles(count) {
+
+        return new Promise((resolve, reject) => {
+          // initiate variables
+          var file = filesArray[fileIndex];
+          var temppath = file.path;
+          var filetype = file.headers['content-type'].split('/');
           var filename = userId + '-' + Date.now() + '.' + filetype[1];
-
           // Build file paths
           var filedir = 'uploads/';
           var filepath = filedir + filename;
@@ -40,32 +44,58 @@ module.exports = {
           var thumbnailPath = filedir + thumbnailName;
           var tempThumbnailPath = 'static/' + filedir + thumbnailName;
 
-          var fileContent = fs.readFileSync(temppath);
+          // read original files
+          fs.readFile(temppath, (err, data) => {
+            if (filetype[0] === 'image') {
+              // upload original image and update entry object
+              entry.filepath = uploadToS3(filepath, data);
+              easyimg.rescrop({
+                 src: temppath, 
+                 dst: tempThumbnailPath,
+                 width:60, 
+                 height:60,
+              })
+              .then((image) => {
+                fs.readFile(tempThumbnailPath, function(err, data) {
+                  if (err) {
+                    reject(err);
+                  }
+                  // Upload to S3 and set thumbnail filepath
+                  entry.thumbnail = uploadToS3(thumbnailPath, data);
+                  // Delete temporary local thumbnail
+                  fs.unlink(tempThumbnailPath, function(err, data) {
+                    if (err) {
+                      console.log('Temporary local thumbnail deletion error', err);
+                    }
+                  })
+                  resolve(entry);
+                })
+              })
+            } else {
+              // else file must be audio
+              entry.audiopath = uploadToS3(filepath, data);
+              resolve(entry);
+            }
+          })
+        })
+        .then((entry) => {
+          return new Promise((resolve, reject) => {
+            if (fileIndex > 0) {
+              fileIndex--;
+              parseFiles(fileIndex);
+            } else {
+              resolve(entry);
+            }
+          })
+        })
+        .then((entry) => {
+          res.json(entry);
+        })
+      }
 
-          // If image then create thumbnail and upload to S3
-          if (filetype[0] === 'image') {
-            // Upload image to S3 and set filepath on entry object
-            entry.filepath =  uploadToS3(filepath, fileContent);
-          } else {
-            // Upload audio file to S3 and set filepath on entry object
-            entry.audiopath =  uploadToS3(filepath, fileContent);
-          }
-          resolve(entry);
-        } // end for in files loop
-      }) // end form.parse
-    }) // end promise
-    .then((entry) => {
-      db.Entry.create(entry)
-      .then((newEntry) => {
-        res.json(newEntry);
-      })
-      .catch((err) => {
-        res.status(500).json(err);
-      })
-    })
-    .catch((err) => {
-      res.status(500).json(err);
-    })
+      parseFiles(fileIndex);
+
+    }) // end form.parse
 
   },
 
